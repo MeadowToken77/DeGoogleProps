6e87719768ebd52129d855ab7750917d2b8ac8d9
MeadowToken77 <278518393+MeadowToken77@users.noreply.github.com>
2026-06-07 15:13:50 +0200

    more bugfixes and refactoring

--- service.sh
+++ service.sh
@@ -1,20 +1,20 @@
-#! /system/bin/sh                                                                       │
-#=====================================================================                  │
-#script to ennsure defined settings are applied                                         │
-#=====================================================================                  │
-chmod 0755 bin/curl │
-│
-## source config variables                                                              │
-source settings.conf │
-│
-## set captive portal                                                                   │
-su -c settings put global captive_portal_enabled $CAPTIVE_PORTAL_ENABLED │
-su -c settings put global captive_portal_http_url $CAPTIVE_PORTAL_HTTP │
-su -c settings put global captive_portal_https_url $CAPTIVE_PORTAL_HTTPS │
-│
-## set private dns                                                                      │
-su -c settings put global private_dns_mode hostname │
-su -c settings put global private_dns_specifier $PRIVATE_DNS │
-│
-## set ntp server                                                                       │
+#! /system/bin/sh
+#=====================================================================
+#script to ennsure defined settings are applied
+#=====================================================================
+chmod 0755 bin/curl
+
+## source config variables
+source settings.conf
+
+## set captive portal
+su -c settings put global captive_portal_enabled $CAPTIVE_PORTAL_ENABLED
+su -c settings put global captive_portal_http_url $CAPTIVE_PORTAL_HTTP
+su -c settings put global captive_portal_https_url $CAPTIVE_PORTAL_HTTPS
+
+## set private dns
+su -c settings put global private_dns_mode hostname
+su -c settings put global private_dns_specifier $PRIVATE_DNS
+
+## set ntp server
 su -c settings put global ntp_server $NTP_SERVER
